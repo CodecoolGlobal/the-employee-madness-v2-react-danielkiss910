95e2a99613b0f7b47b2545968a3c3bce9082bebf
danielkiss910 <danielkiss910@gmail.com>
2023-08-18 08:57:33 +0200

    Extend list with filter features

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,36 +1,78 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 
-const EmployeeTable = ({ employees, onDelete }) => (
-  <div className="EmployeeTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Level</th>
-          <th>Position</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee._id}>
-            <td>{employee.name}</td>
-            <td>{employee.level}</td>
-            <td>{employee.position}</td>
-            <td>
-              <Link to={`/update/${employee._id}`}>
-                <button type="button">Update</button>
-              </Link>
-              <button type="button" onClick={() => onDelete(employee._id)}>
-                Delete
-              </button>
-            </td>
+const EmployeeTable = ({ employees, onDelete }) => {
+
+  const [positionFilter, setPositionFilter] = useState("");
+  const [levelFilter, setLevelFilter] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredEmployees = employees.filter((employee) => {
+    const matchesPosition = !positionFilter || employee.position.toLowerCase().includes(positionFilter.toLowerCase());
+    const matchesLevel = !levelFilter || employee.level.toLowerCase().includes(levelFilter.toLowerCase());
+    const matchesSearch = 
+      employee.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      employee.position.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      employee.level.toLowerCase().includes(searchQuery.toLowerCase());
+
+    return matchesPosition && matchesLevel && matchesSearch;
+  });
+
+  
+  return (
+    <div className="EmployeeTable">
+      <div className="filters">
+        <input
+          type="text"
+          placeholder="Filter by Position"
+          value={positionFilter}
+          onChange={(e) => setPositionFilter(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Filter by Level"
+          value={levelFilter}
+          onChange={(e) => setLevelFilter(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />        
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Position</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {filteredEmployees.map((employee) => (
+            <tr key={employee._id}>
+              <td>{employee.name}</td>
+              <td>{employee.level}</td>
+              <td>{employee.position}</td>
+              <td>
+                <Link to={`/update/${employee._id}`}>
+                  <button type="button">
+                    Update
+                  </button>
+                </Link>
+                  <button type="button" onClick={() => onDelete(employee._id)}>
+                    Delete
+                  </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default EmployeeTable;
