64190a8f8f977377ee5c3a77f945aabb8dd31eeb
danielkiss910 <danielkiss910@gmail.com>
2023-08-23 11:45:17 +0200

    Equipment page with routing created

---
+++ client/src/Pages/EquipmentList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Loading from "../Components/Loading";
+
+const EquipmentList = () => {
+
+
+return (
+    <div>
+        <h2>Hello</h2>
+        <h3>bello</h3>
+    </div>
+)
+};
+
+
+export default EquipmentList;
