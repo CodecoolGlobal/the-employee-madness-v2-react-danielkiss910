3b8df0a6883f89edc131206d097d4d0bab181449
danielkiss910 <danielkiss910@gmail.com>
2023-09-10 00:27:33 +0200

    Present employees checkbox functional on EmpTable

--- client/src/Pages/EmployeeList.jsx
+++ client/src/Pages/EmployeeList.jsx
@@ -2,26 +2,38 @@ import { useEffect, useState } from "react";
 import Loading from "../Components/Loading";
 import EmployeeTable from "../Components/EmployeeTable";
 
-const fetchEmployees = () => {
-  return fetch("/api/employees").then((res) => res.json());
+const fetchEmployees = async () => {
+  const res = await fetch("/api/employees");
+  return await res.json();
 };
 
-const deleteEmployee = (id) => {
-  return fetch(`/api/employees/${id}`, { method: "DELETE" }).then((res) =>
-    res.json()
-  );
+const deleteEmployee = async (id) => {
+  const res = await fetch(`/api/employees/${id}`, { method: "DELETE" });
+  return await res.json();
 };
 
 const EmployeeList = () => {
   const [loading, setLoading] = useState(true);
   const [employees, setEmployees] = useState(null);
+  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleDelete = (id) => {
-    deleteEmployee(id);
-
-    setEmployees((employees) => {
-      return employees.filter((employee) => employee._id !== id);
-    });
+  const handleDelete = async (id) => {
+    setLoading(true);
+    try {
+      const result = await deleteEmployee(id);
+      if (result.success) {
+        setEmployees((employees) => {
+          return employees.filter((employee) => employee._id !== id);
+        });
+      } else {
+        setErrorMessage("Failed to delete employee. Please try again.");
+      }
+    } catch (error) {
+      setErrorMessage("Failed to delete employee due to a network error. Please try again.")
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -30,13 +42,30 @@ const EmployeeList = () => {
         setLoading(false);
         setEmployees(employees);
       })
+      .catch((err) => {
+        setLoading(false);
+        setError("Failed to delete employee. Please try again.");
+      });
   }, []);
 
   if (loading) {
     return <Loading />;
   }
 
-  return <EmployeeTable employees={employees} onDelete={handleDelete} />;
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  return (
+    <div>
+      {errorMessage && <p className="error">{errorMessage}</p>}
+      <EmployeeTable 
+        employees={employees} 
+        setEmployees={setEmployees}
+        onDelete={handleDelete} 
+        />
+    </div>
+  );
 };
 
 export default EmployeeList;
