46d6eaea31b4cc4d6db73d775ffbdea3e3e6e684
Daniel Kiss <danielkiss910@gmail.com>
2024-05-23 16:23:57 +0200

    Refactor: add error handling for errorpage component

--- client/src/Pages/ErrorPage.jsx
+++ client/src/Pages/ErrorPage.jsx
@@ -8,9 +8,11 @@ const ErrorPage = () => {
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      {error && (
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      )}
     </div>
   );
 };
