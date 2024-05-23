809c667561166bb96be0537fd8341ccbeab27fac
Daniel Kiss <danielkiss910@gmail.com>
2024-05-23 15:49:09 +0200

    Fix name rendering issue, update employee form with prop validation

--- client/src/Components/EmployeeForm/EmployeeForm.jsx
+++ client/src/Components/EmployeeForm/EmployeeForm.jsx
@@ -1,38 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const EmployeeForm = ({ onSave, disabled, employee, onCancel }) => {
-  const [name, setName] = useState(employee?.name ?? "");
-  const [level, setLevel] = useState(employee?.level ?? "");
-  const [position, setPosition] = useState(employee?.position ?? "");
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [level, setLevel] = useState("");
+  const [position, setPosition] = useState("");
+
+  useEffect(() => {
+    if (employee) {
+      setFirstName(employee.firstName || "");
+      setMiddleName(employee.middleName || "");
+      setLastName(employee.lastName || "");
+      setLevel(employee.level || "");
+      setPosition(employee.position || "");
+    }
+  }, [employee]);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (employee) {
-      return onSave({
-        ...employee,
-        name,
-        level,
-        position,
-      });
-    }
-
-    return onSave({
-      name,
+    const employeeData = {
+      firstName,
+      middleName,
+      lastName,
       level,
       position,
-    });
+    };
+
+    if (employee) {
+      employeeData._id = employee._id;
+    }
+
+    onSave(employeeData);
   };
 
   return (
     <form className="EmployeeForm" onSubmit={onSubmit}>
       <div className="control">
-        <label htmlFor="name">Name:</label>
+        <label htmlFor="firstName">First Name:</label>
         <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          name="name"
-          id="name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          name="firstName"
+          id="firstName"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="middleName">Middle Name:</label>
+        <input
+          value={middleName}
+          onChange={(e) => setMiddleName(e.target.value)}
+          name="middleName"
+          id="middleName"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          name="lastName"
+          id="lastName"
         />
       </div>
 
@@ -60,13 +92,31 @@ const EmployeeForm = ({ onSave, disabled, employee, onCancel }) => {
         <button type="submit" disabled={disabled}>
           {employee ? "Update Employee" : "Create Employee"}
         </button>
-
         <button type="button" onClick={onCancel}>
           Cancel
         </button>
       </div>
     </form>
   );
+};
+
+EmployeeForm.propTypes = {
+  onSave: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  employee: PropTypes.shape({
+    _id: PropTypes.string,
+    firstName: PropTypes.string,
+    middleName: PropTypes.string,
+    lastName: PropTypes.string,
+    level: PropTypes.string,
+    position: PropTypes.string,
+  }),
+  onCancel: PropTypes.func.isRequired,
+};
+
+EmployeeForm.defaultProps = {
+  disabled: false,
+  employee: null,
 };
 
 export default EmployeeForm;
